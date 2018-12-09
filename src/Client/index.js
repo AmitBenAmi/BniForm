65ad1413b4ecab1e4d4b709560f6b57f1759345d
AmitBenAmi <benamiamit0@gmail.com>
2018-12-09 23:04:36 +0200

    Changing import and using the members service

--- src/Client/index.js
+++ src/Client/index.js
@@ -1,15 +1,19 @@
 import 'bootstrap-v4-rtl'
 import 'bootstrap-v4-rtl/dist/css/bootstrap-rtl.css'
 import 'popper.js'
-import 'CSS/bootstrap-override.css'
-import 'CSS/style.css'
+import 'Styles/bootstrap-override.css'
+import 'Styles/style.css'
 import myBody from 'HTML/body.html'
+import MembersService from 'Services/members-service'
 
 $('body').append(myBody)
+
+let membersService = new MembersService()
 
 window.addEventListener('hashchange', () => {
     let fragment = window.location.hash.replace(/^#/, '')
     if (fragment === 'members') {
+        let members = membersService.getAllMembers()
         $('#form-container').hide()
         $('#members-container').show()
     }
