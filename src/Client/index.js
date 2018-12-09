2a87f5220b87965729956159b8266cce8becb7e3
AmitBenAmi <benamiamit0@gmail.com>
2018-12-09 22:35:07 +0200

    Toggling between the form and the members

--- src/Client/index.js
+++ src/Client/index.js
@@ -2,6 +2,19 @@ import 'bootstrap-v4-rtl'
 import 'bootstrap-v4-rtl/dist/css/bootstrap-rtl.css'
 import 'popper.js'
 import 'CSS/bootstrap-override.css'
+import 'CSS/style.css'
 import myBody from 'HTML/body.html'
 
 $('body').append(myBody)
+
+window.addEventListener('hashchange', () => {
+    let fragment = window.location.hash.replace(/^#/, '')
+    if (fragment === 'members') {
+        $('#form-container').hide()
+        $('#members-container').show()
+    }
+    else if (fragment === 'form') {
+        $('#form-container').show()
+        $('#members-container').hide()
+    }
+}, false)
