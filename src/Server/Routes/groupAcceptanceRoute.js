c8496c5b5ac2c04a07fbe30c2ae502df435f8a57
AmitBenAmi <benamiamit0@gmail.com>
2019-02-06 11:50:42 +0200

    Setting the endpoint for getting a group acceptance

--- src/Server/Routes/groupAcceptanceRoute.js
+++ src/Server/Routes/groupAcceptanceRoute.js
@@ -4,6 +4,9 @@ class GroupAcceptanceRoute {
     static async init(app, db) {
         this.groupAcceptanceController = new GroupAcceptanceController(db)
         this.groupAcceptanceController.init()
+
+        app.route('/acceptance/:acceptanceId')
+            .get(this.groupAcceptanceController.getGroupAcceptance)
     }
 }
 
